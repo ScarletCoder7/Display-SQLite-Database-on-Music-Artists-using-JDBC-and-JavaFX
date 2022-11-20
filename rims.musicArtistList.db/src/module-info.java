1dbb2f3fe12fe373df17026475979cb86050d322
Shrinjit <88984475+ScarletCoder7@users.noreply.github.com>
2022-11-20 15:23:56 +0530

    Update module-info.java

--- rims.musicArtistList.db/src/module-info.java
+++ rims.musicArtistList.db/src/module-info.java
@@ -1,6 +1,6 @@
 module rims.musicArtistList.db {
     requires java.sql;
     requires sqlite.jdbc;
-    requires transitive rims.musicArtistList.common;  // So the ui module can have access to all export packages and ui module will not be required the common module
+    requires transitive rims.musicArtistList.common;  
     exports rims.musicArtistList.db;
 }
