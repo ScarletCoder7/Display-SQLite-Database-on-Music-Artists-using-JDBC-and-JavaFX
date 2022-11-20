5ce7e65fd6f80f318fa79ae8254c4ef5c1ba3b22
Shrinjit <88984475+ScarletCoder7@users.noreply.github.com>
2022-11-20 15:23:11 +0530

    Update module-info.java

--- rims.musicArtistList.ui/src/module-info.java
+++ rims.musicArtistList.ui/src/module-info.java
@@ -3,7 +3,7 @@ module rims.musicArtistList.ui {
     requires javafx.graphics;
     requires javafx.fxml;
     requires javafx.controls;
-    //requires rims.musicArtistList.common; // Since requires transitive rims.musicArtistList.common; in db module so in ui module we don't need this line anymore
+    
     requires rims.musicArtistList.db;
 
     exports rims.musicArtistList.ui to javafx.graphics;
